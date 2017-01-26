0b9174acc6a224bda953ae7836bd6ba0aed023bd
vizvamitra <vizvamitra@gmail.com>
2017-01-26 09:53:13 +0300

    Fix travis builds and failing specs

--- lib/uploadcare/api/group_list_api.rb
+++ lib/uploadcare/api/group_list_api.rb
@@ -1,7 +1,7 @@
 module Uploadcare
   module GroupListApi
     def group_list from=nil, limit=nil
-      data = get '/groups/', {from: from, limit: limit}
+      data = get '/groups/', {from: from, limit: limit}.reject{|_,v| v.nil?}
       list = Uploadcare::Api::GroupList.new self, data
     end
   end
