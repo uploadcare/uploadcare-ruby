9a905828c1fd0dabb0f40c183d850b3c66aef391
Lidiane Taquehara <lidi.mayra@gmail.com>
2017-06-14 00:36:03 -0300

    Refactor raw_api specs
    Refactor raw_api specs following Better Specs guidelines.
    Ref: http://www.betterspecs.org/
    
    Applied concepts include:
      - Use `expect` syntax instead of `should`
      - Use subject
      - Use let and let!
    
    Also: included examples of how a returned hash looks like.
    
    This refactor aims to improve specs as documentation, providing
    a clearer understanding of api behaviour.

--- spec/api/raw_api_spec.rb
+++ spec/api/raw_api_spec.rb
@@ -3,23 +3,74 @@ require 'uri'
 require 'socket'
 
 describe Uploadcare::Api do
-  before :each do
-    @api = Uploadcare::Api.new(CONFIG)
-  end
+  subject(:api) { Uploadcare::Api.new(CONFIG) }
 
   it "should initialize api" do
-    @api.should be_an_instance_of(Uploadcare::Api)
+    is_expected.to be_an_instance_of(Uploadcare::Api)
   end
 
   it 'should respond to request methods' do
-    @api.should respond_to :request
-    @api.should respond_to :get
-    @api.should respond_to :post
-    @api.should respond_to :put
-    @api.should respond_to :delete
+    is_expected.to respond_to :request
+    is_expected.to respond_to :get
+    is_expected.to respond_to :post
+    is_expected.to respond_to :put
+    is_expected.to respond_to :delete
   end
 
-  it 'should perform custom requests' do
-    expect { @api.request }.to_not raise_error
+  context 'when performing requests' do
+    subject(:request) { api.request }
+
+    it { is_expected.to be_a Hash }
+
+    context 'when checking keys' do
+      subject { request.keys }
+
+      it { is_expected.to eq %w(next previous total per_page results) }
+    end
+
+    context 'when checking next value' do
+      let(:request_next) { 'https://api.uploadcare.com/files' }
+
+      subject { request['next'] }
+
+      it { is_expected.to match request_next }
+    end
+
+    context 'when checking previous value' do
+      subject { request['previous'] }
+
+      it { is_expected.to be_nil }
+    end
+
+    context 'when checking total value' do
+      subject { request['previous'] }
+
+      it { is_expected.to be_nil }
+    end
+
+    context 'when checking per_page value' do
+      subject { request['per_page'] }
+
+      it { is_expected.to be 100 }
+    end
+
+    context 'when checking results' do
+      subject(:results) { request['results'] }
+
+      it { expect(results.count).to be 100 }
+
+      context 'when checking one result' do
+        let(:keys) do
+          %w(uuid original_file_url image_info datetime_stored mime_type
+             is_ready url original_filename datetime_uploaded size is_image
+             datetime_removed source)
+        end
+
+        subject(:result) { results.sample }
+
+        it { is_expected.to be_a Hash }
+        it { expect(result.keys).to eq keys }
+      end
+    end
   end
 end
