fb7cd9a59ce71687940ed7cca3c882fe0ee52151
Lidiane Taquehara <lidi.mayra@gmail.com>
2017-06-17 17:00:23 -0300

    Remove specs that check for hash's content in raw api

--- spec/api/raw_api_spec.rb
+++ spec/api/raw_api_spec.rb
@@ -21,56 +21,5 @@ describe Uploadcare::Api do
     subject(:request) { api.request }
 
     it { is_expected.to be_a Hash }
-
-    context 'when checking keys' do
-      subject { request.keys }
-
-      it { is_expected.to eq %w(next previous total per_page results) }
-    end
-
-    context 'when checking next value' do
-      let(:request_next) { 'https://api.uploadcare.com/files' }
-
-      subject { request['next'] }
-
-      it { is_expected.to match request_next }
-    end
-
-    context 'when checking previous value' do
-      subject { request['previous'] }
-
-      it { is_expected.to be_nil }
-    end
-
-    context 'when checking total value' do
-      subject { request['previous'] }
-
-      it { is_expected.to be_nil }
-    end
-
-    context 'when checking per_page value' do
-      subject { request['per_page'] }
-
-      it { is_expected.to be 100 }
-    end
-
-    context 'when checking results' do
-      subject(:results) { request['results'] }
-
-      it { expect(results.count).to be 100 }
-
-      context 'when checking one result' do
-        let(:keys) do
-          %w(uuid original_file_url image_info datetime_stored mime_type
-             is_ready url original_filename datetime_uploaded size is_image
-             datetime_removed source)
-        end
-
-        subject(:result) { results.sample }
-
-        it { is_expected.to be_a Hash }
-        it { expect(result.keys).to eq keys }
-      end
-    end
   end
 end
