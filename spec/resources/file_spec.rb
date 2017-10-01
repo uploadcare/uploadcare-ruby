a7a8f7e102fffb37c36bd26b9683225faedaafe2
Lidiane Taquehara <lidi.mayra@gmail.com>
2017-09-30 22:44:01 -0300

    Remove old `should` syntax from file spec
    Usage of old syntax was raising Deprecation Warnings.
    At the present moment, recommended syntax is `expect` instead of
    `should`

--- spec/resources/file_spec.rb
+++ spec/resources/file_spec.rb
@@ -9,44 +9,44 @@ describe Uploadcare::Api::File do
     @file = @api.upload IMAGE_URL
   end
 
-  it 'file should be an instance of File' do
-    @file.should be_an_instance_of Uploadcare::Api::File
+  it 'is an instance of File' do
+    expect(@file).to be_an_instance_of Uploadcare::Api::File
   end
 
-  it 'should not be initialized without correct UUID given' do
+  it 'is not initialized without correct UUID given' do
     expect {Uploadcare::Api::File.new(@api, "not-uuid")}.to raise_error
   end
 
-  it 'should have valid url' do
-    @file.uuid.should match UUID_REGEX
+  it 'has a valid url' do
+    expect(@file.uuid).to match UUID_REGEX
   end
 
-  it 'should return public url' do
-    @file.should respond_to :cdn_url
-    @file.should respond_to :public_url
+  it 'returns a public url' do
+    expect(@file).to respond_to :cdn_url
+    expect(@file).to respond_to :public_url
   end
 
-  it 'public url should be valid url' do
+  it 'has a public url that is valid' do
     url = @file.cdn_url
     uri = URI.parse(url)
-    uri.should be_kind_of(URI::HTTP)
+    expect(uri).to be_kind_of(URI::HTTP)
   end
 
-  it 'should be able to load image data' do
+  it 'is able to load image data' do
     expect {@file.load_data}.to_not raise_error
   end
 
-  it 'should store itself' do
+  it 'stores itself' do
     expect { @file.store }.to_not raise_error
   end
 
-  it 'file should respond with nil for :stored? and :deleted? methods unless loaded' do
-    @file.is_loaded?.should == false
-    @file.is_stored?.should == nil
-    @file.is_deleted?.should == nil
+  it 'responds with nil for :stored? and :deleted? methods unless loaded' do
+    expect(@file.is_loaded?).to be false
+    expect(@file.is_stored?).to be_nil
+    expect(@file.is_deleted?).to be_nil
   end
 
-  it 'should be able to tell thenever file was stored' do
+  it 'is able to tell thenever file was stored' do
     @file.load
     expect(@file.stored?).to be(true)
     wait_until_ready(@file)
@@ -54,64 +54,64 @@ describe Uploadcare::Api::File do
     expect(@file.stored?).to be(false)
   end
 
-  it 'should delete itself' do
+  it 'deletes itself' do
     expect { @file.delete }.to_not raise_error
   end
 
-  it 'should be able to tell thenever file was deleted' do
+  it 'is able to tell thenever file was deleted' do
     @file.load
     @file.is_deleted?.should == false
     wait_until_ready(@file)
     @file.delete
-    @file.is_deleted?.should == true
+    expect(@file.is_deleted?).to be true
   end
 
-  it 'should construct file from uuid' do
+  it 'constructs file from uuid' do
     file = @api.file @file.uuid
-    file.should be_kind_of(Uploadcare::Api::File)
+    expect(file).to be_kind_of(Uploadcare::Api::File)
   end
 
-  it 'should construct file from cdn url' do
+  it 'constructs file from cdn url' do
     url = @file.cdn_url + "-/crop/150x150/center/-/format/png/"
     file = @api.file url
-    file.should be_kind_of(Uploadcare::Api::File)
+    expect(file).to be_kind_of(Uploadcare::Api::File)
   end
 
-  it 'shoul respond to datetime_ methods' do
+  it 'responds to datetime_ methods' do
     @file.load
-    @file.should respond_to(:datetime_original)
-    @file.should respond_to(:datetime_uploaded)
-    @file.should respond_to(:datetime_stored)
-    @file.should respond_to(:datetime_removed)
+    expect(@file).to respond_to(:datetime_original)
+    expect(@file).to respond_to(:datetime_uploaded)
+    expect(@file).to respond_to(:datetime_stored)
+    expect(@file).to respond_to(:datetime_removed)
   end
 
-  it 'should respond to datetime_uploaded' do
+  it 'responds to datetime_uploaded' do
     @file.load
-    @file.datetime_uploaded.should be_kind_of(DateTime)
+    expect(@file.datetime_uploaded).to be_kind_of(DateTime)
   end
 
-  it 'should respond to datetime_stored' do
+  it 'responds to datetime_stored' do
     @file.load
     @file.store
-    @file.datetime_stored.should be_kind_of(DateTime)
+    expect(@file.datetime_stored).to be_kind_of(DateTime)
   end
 
-  it 'should respond to datetime_removed' do
+  it 'responds to datetime_removed' do
     @file.load
     wait_until_ready(@file)
     @file.delete
-    @file.datetime_removed.should be_kind_of(DateTime)
-    @file.datetime_deleted.should be_kind_of(DateTime)
-    @file.datetime_removed.should == @file.datetime_deleted
+    expect(@file.datetime_removed).to be_kind_of(DateTime)
+    expect(@file.datetime_deleted).to be_kind_of(DateTime)
+    expect(@file.datetime_removed).to eq @file.datetime_deleted
   end
 
 
-  it 'should copy itself' do
+  it 'copies itself' do
     # This can cause "File is not ready yet" error if ran too early
     # In this case we retry it 3 times before giving up
     result = retry_if(Uploadcare::Error::RequestError::BadRequest){@file.copy}
-    result.should be_kind_of(Hash)
-    result["type"].should == "file"
+    expect(result).to be_kind_of(Hash)
+    expect(result["type"]).to eq "file"
   end
 
 
