7e4b582473a97a5629c8919c243e5c0c462b5e00
Stepan Redka <stepan@railsmuffin.com>
2020-01-20 13:08:17 +0300

    refactor: spec class naming

--- spec/uploadcare/entity/file_list_spec.rb
+++ spec/uploadcare/entity/file_list_spec.rb
@@ -4,13 +4,13 @@ module Uploadcare
   RSpec.describe FileList do
     it 'responds to expected methods' do
       %i[file_list].each do |method|
-        expect(Uploadcare::FileList).to respond_to(method)
+        expect(FileList).to respond_to(method)
       end
     end
 
     it 'represents a file as entity' do
       VCR.use_cassette('rest_file_list') do
-        file_list = Uploadcare::FileList.file_list
+        file_list = FileList.file_list
         %i[next previous results total].each do |method|
           expect(file_list).to respond_to(method)
         end
