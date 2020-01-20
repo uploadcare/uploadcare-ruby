445bd36326748066b991198e312afb6ddabf5cfb
Stepan Redka <stepan@railsmuffin.com>
2020-01-20 11:33:37 +0300

    test: better file specs

--- spec/uploadcare/client/file_client_spec.rb
+++ spec/uploadcare/client/file_client_spec.rb
@@ -2,10 +2,59 @@ require 'spec_helper'
 
 module Uploadcare
   RSpec.describe FileClient do
-    it 'makes a request' do
-      VCR.use_cassette('file') do
-        response = FileClient.new.index
-        expect(WebMock).to have_requested(:get, "https://api.uploadcare.com/files/")
+    subject { FileClient.new }
+    
+    describe 'index' do
+      before do
+        VCR.use_cassette('file') do
+          @files = subject.index.value!
+        end
+      end
+
+      it 'lists a bunch of files' do
+        expect(@files.length).to eq(3)
+      end
+    end
+
+    describe 'info' do
+      it 'shows insider info about that file' do
+        VCR.use_cassette('file_info') do
+          uuid = '8f64f313-e6b1-4731-96c0-6751f1e7a50a'
+          file = subject.info(uuid)
+          expect(file.value![:uuid]).to eq(uuid)
+        end
+      end
+    end
+
+    describe 'copy' do
+      it 'makes a copy of a file' do
+        VCR.use_cassette('rest_file_copy') do
+          uuid = '8f64f313-e6b1-4731-96c0-6751f1e7a50a'
+          response = subject.copy(source: uuid)
+          new_uuid = response.value!.dig(:result, :uuid)
+          expect(new_uuid).to be_a_kind_of(String)
+          expect(new_uuid).not_to eq(uuid)
+        end
+      end
+
+      it 'accepts other arguments' do
+        VCR.use_cassette('rest_file_copy_arg') do
+          uuid = '8f64f313-e6b1-4731-96c0-6751f1e7a50a'
+          response = subject.copy(source: uuid, target: 'nowhere')
+          expect(response.to_s).to include('Project has no storage with provided name.')
+        end
+      end
+    end
+
+    describe 'delete' do
+      it 'deletes a file' do
+        VCR.use_cassette('rest_file_delete') do
+          uuid = 'e9a9f291-cc52-4388-bf65-9feec1c75ff9'
+          response = subject.delete(uuid)
+          response_value = response.value!
+          expect(response_value[:datetime_removed]).not_to be_empty
+          expect(response_value[:uuid]).to eq(uuid)
+        end
       end
     end
   end
