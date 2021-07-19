f05a5b926902a7ef68da9ad2dc8c2b1786b3f4dc
dmitrij <dmitrij.ivanchenko@gmail.com>
2021-07-19 13:47:19 +0600

    Add spec to check if the video original_source includes all the params

--- spec/uploadcare/entity/video_converter_spec.rb
+++ spec/uploadcare/entity/video_converter_spec.rb
@@ -9,28 +9,31 @@ module Uploadcare
 
       describe 'convert_many' do
         shared_examples 'converts videos' do
-          it 'returns a result with video data' do
+          it 'returns a result with video data', :aggregate_failures do
             VCR.use_cassette('video_convert_convert_many') do
               response_value = subject.convert(array_of_params, **options)
               result = response_value[:result].first
 
               expect(response_value[:problems]).to be_empty
               expect(result[:uuid]).not_to be_nil
+
+              [video_params[:uuid], :size, :quality, :format, :cut, :thumbs].each do |param|
+                expect(result[:original_source]).to match(param.to_s)
+              end
             end
           end
         end
 
-        let(:array_of_params) do
-          [
-            {
-              uuid: 'e30112d7-3a90-4931-b2c5-688cbb46d3ac',
-              size: { resize_mode: 'change_ratio', width: '600', height: '400' },
-              quality: 'best',
-              format: 'ogg',
-              cut: { start_time: '0:0:0.0', length: '0:0:1.0' },
-              thumbs: { thumbs_n: 2, number: 1 }
-            }
-          ]
+        let(:array_of_params) { [video_params] }
+        let(:video_params) do
+          {
+            uuid: 'e30112d7-3a90-4931-b2c5-688cbb46d3ac',
+            size: { resize_mode: 'change_ratio', width: '600', height: '400' },
+            quality: 'best',
+            format: 'ogg',
+            cut: { start_time: '0:0:0.0', length: '0:0:1.0' },
+            thumbs: { thumbs_n: 2, number: 1 }
+          }
         end
         let(:options) { { store: false } }
 
