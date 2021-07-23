9db66a1d626211d181c9077279b7cfd668a531ef
dmitrij <dmitrij.ivanchenko@gmail.com>
2021-07-23 17:03:41 +0600

    Add description for conversion clients methods

--- lib/uploadcare/entity/document_converter.rb
+++ lib/uploadcare/entity/document_converter.rb
@@ -6,17 +6,18 @@ module Uploadcare
     # @see https://uploadcare.com/api-refs/rest-api/v0.6.0/#operation/documentConvert
     class DocumentConverter < Entity
       client_service Conversion::DocumentConversionClient
-
-      # Upload file or group of files from array, File, or url
+      # Converts documents
       #
-      # @param object [Array] of hashes with params
-      # @param [Hash] of options for conversion
+      # @param doc_params [Array] of hashes with params or [Hash]
       # @option options [Boolean] :store (false) whether to store file on servers.
       def self.convert(doc_params, **options)
         params = doc_params.is_a?(Hash) ? [doc_params] : doc_params
         Conversion::DocumentConversionClient.new.convert_many(params, **options)
       end
 
+      # Returns a status of document conversion job
+      #
+      # @param token [Integer, String] token obtained from a server in convert method
       def self.status(token)
         Conversion::DocumentConversionClient.new.get_conversion_status(token)
       end
--- lib/uploadcare/entity/video_converter.rb
+++ lib/uploadcare/entity/video_converter.rb
@@ -6,17 +6,18 @@ module Uploadcare
     # @see https://uploadcare.com/api-refs/rest-api/v0.6.0/#operation/videoConvert
     class VideoConverter < Entity
       client_service Conversion::VideoConversionClient
-
-      # Upload file or group of files from array, File, or url
+      # Converts video files
       #
-      # @param object [Array] of hashes with params
-      # @param [Hash] of options for conversion
+      # @param doc_params [Array] of hashes with params or [Hash]
       # @option options [Boolean] :store (false) whether to store file on servers.
       def self.convert(video_params, **options)
         params = video_params.is_a?(Hash) ? [video_params] : video_params
         Conversion::VideoConversionClient.new.convert_many(params, **options)
       end
 
+      # Returns a status of video conversion job
+      #
+      # @param token [Integer, String] token obtained from a server in convert method
       def self.status(token)
         Conversion::VideoConversionClient.new.get_conversion_status(token)
       end
