26dd26173ec6b39bf8a5a42e8d7d09163a90a817
dmitrij <dmitrij.ivanchenko@gmail.com>
2021-07-23 13:51:28 +0600

    Add specs for failed document conversion

--- spec/uploadcare/client/conversion/document_conversion_client_spec.rb
+++ spec/uploadcare/client/conversion/document_conversion_client_spec.rb
@@ -6,41 +6,76 @@ module Uploadcare
   module Client
     module Conversion
       RSpec.describe DocumentConversionClient do
-        describe 'convert_many' do
-          subject { described_class.new.convert_many(array_of_params, **options) }
+        describe 'successfull conversion' do
+          describe 'convert_many' do
+            subject { described_class.new.convert_many(array_of_params, **options) }
 
-          shared_examples 'requesting documents conversion' do
-            it 'returns a convert documents response' do
-              VCR.use_cassette('document_convert_convert_many') do
+            shared_examples 'succeeds documents conversion' do
+              it 'returns a convert documents response' do
+                VCR.use_cassette('document_convert_convert_many') do
+                  expect(subject.success?).to be true
+                end
+              end
+            end
+
+            let(:array_of_params) do
+              [
+                {
+                  uuid: 'a4b9db2f-1591-4f4c-8f68-94018924525d',
+                  format: 'png',
+                  page: 1
+                }
+              ]
+            end
+            let(:options) { { store: false } }
+
+            context 'when all params are present' do
+              it_behaves_like 'succeeds documents conversion'
+            end
+          end
+
+          describe 'get document conversion status' do
+            subject { described_class.new.get_conversion_status(token) }
+
+            let(:token) { '21120333' }
+
+            it 'returns a document conversion status data' do
+              VCR.use_cassette('document_convert_get_status') do
                 expect(subject.success?).to be true
               end
             end
           end
-
-          let(:array_of_params) do
-            [
-              {
-                uuid: 'a4b9db2f-1591-4f4c-8f68-94018924525d',
-                format: 'png',
-                page: 1
-              }
-            ]
-          end
-          let(:options) { { store: false } }
-
-          context 'when all params are present' do
-            it_behaves_like 'requesting documents conversion'
-          end
         end
 
-        describe 'get document conversion status' do
-          subject { described_class.new.get_conversion_status(token) }
+        describe 'conversion with error' do
+          shared_examples 'failed document conversion' do
+            it 'raises a conversion error' do
+              VCR.use_cassette('document_convert_convert_many_with_error') do
+                expect { subject }.to raise_error(
+                  Uploadcare::Exception::ConversionError, message
+                )
+              end
+            end
+          end
 
-          let(:token) { '21120333' }
+          describe 'convert_many' do
+            subject { described_class.new.convert_many(array_of_params, **options) }
 
-          it 'returns a document conversion status data' do
-            VCR.use_cassette('document_convert_get_status') do
-              expect(subject.success?).to be true
+            let(:array_of_params) do
+              [
+                {
+                  uuid: '86c54d9a-3453-4b12-8dcc-49883ae8f084',
+                  format: 'jpg',
+                  page: 1
+                }
+              ]
+            end
+            let(:options) { { store: false } }
+
+            context 'when the target_format is not a supported' do
+              let(:message) { /target_format is not a supported/ }
+
+              it_behaves_like 'failed document conversion'
             end
           end
         end
