50f41193af57db8cd8006294e35a26270353e93e
dmitrijivanchenko <dmitrij.ivanchenko@gmail.com>
2021-07-23 12:17:47 +0600

    ID-4:  Video converter and client (#72)
    * Add :cut parameters validator specs
    
    * Add :format parameters validator specs
    
    * Add :quality parameters validator specs
    
    * Add :size parameters validator specs
    
    * Add :store parameters validator specs
    
    * Add :thumbs parameters validator specs
    
    * Add :uuid parameters validator specs
    
    * Add base parameters validator
    
    * Add :cut parameters validator
    
    * Add :format parameters validator
    
    * Add :quality parameters validator
    
    * Add :size parameters validator
    
    * Add :store parameters validator
    
    * Add :thumbs parameters validator
    
    * Add :uuid parameters validator
    
    * Add supported video options yaml file
    
    * Add processing job url builder specs
    
    * Add processing job url builder
    
    * Add validation error
    
    * Add specs for converter and client
    
    * Add conversion client
    
    * Add video converter
    
    * Fix rubocop offenses
    
    * Fix transform keys method for ruby 2.4
    
    * Disable Style/HashTransformKeys as ruby 2.4 has no method 'transform_keys'
    
    * Disable cop Metrics/ParameterLists for ProcessingJobUrlBuilder
    
    * Replace :slice method with :select for ruby 2.4
    
    * Fix specs and rubocop offenses
    
    * Replace non-existing methods for ruby 2.4
    
    * Add spec to check if the video original_source includes all the params
    
    * Remove param validators
    
    * Remove validators from VideoConversionClient
    
    * Allow hash param for single video processing
    
    * Fix conflicts and specs

---
+++ spec/uploadcare/entity/video_converter_spec.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+module Uploadcare
+  module Entity
+    RSpec.describe VideoConverter do
+      subject { Uploadcare::Entity::VideoConverter }
+
+      describe 'convert_many' do
+        shared_examples 'converts videos' do
+          it 'returns a result with video data', :aggregate_failures do
+            VCR.use_cassette('video_convert_convert_many') do
+              response_value = subject.convert(array_of_params, **options).success
+              result = response_value[:result].first
+
+              expect(response_value[:problems]).to be_empty
+              expect(result[:uuid]).not_to be_nil
+
+              [video_params[:uuid], :size, :quality, :format, :cut, :thumbs].each do |param|
+                expect(result[:original_source]).to match(param.to_s)
+              end
+            end
+          end
+        end
+
+        let(:array_of_params) { [video_params] }
+        let(:video_params) do
+          {
+            uuid: 'e30112d7-3a90-4931-b2c5-688cbb46d3ac',
+            size: { resize_mode: 'change_ratio', width: '600', height: '400' },
+            quality: 'best',
+            format: 'ogg',
+            cut: { start_time: '0:0:0.0', length: '0:0:1.0' },
+            thumbs: { thumbs_n: 2, number: 1 }
+          }
+        end
+        let(:options) { { store: false } }
+
+        context 'when all params are present' do
+          it_behaves_like 'converts videos'
+        end
+
+        %i[size quality format cut thumbs].each do |param|
+          context "when only :#{param} param is present" do
+            let(:arguments) { super().select { |k, _v| [:uuid, param].include?(k) } }
+
+            it_behaves_like 'converts videos'
+          end
+        end
+      end
+    end
+  end
+end
