48bdaa512bf70cfd0f65dd1f1641034b71c49b4a
dmitrijivanchenko <dmitrij.ivanchenko@gmail.com>
2021-07-26 17:39:24 +0600

    ID-4: Add request for video conversion status with specs (#73)
    * Add :cut parameters validator specs
    
    * Add :format parameters validator specs
    
    * Add :quality parameters validator specs
    
    * Add :size parameters validator specs
    
    * Add :store parameters validator specs
    
    * Add :thumbs parameters validator specs
    
    * Add :uuid parameters validator specs
    
    * Add base parameters validator
    
    * Add :cut parameters validator
    
    * Add :format parameters validator
    
    * Add :quality parameters validator
    
    * Add :size parameters validator
    
    * Add :store parameters validator
    
    * Add :thumbs parameters validator
    
    * Add :uuid parameters validator
    
    * Add supported video options yaml file
    
    * Add processing job url builder specs
    
    * Add processing job url builder
    
    * Add validation error
    
    * Add specs for converter and client
    
    * Add conversion client
    
    * Add video converter
    
    * Fix rubocop offenses
    
    * Fix transform keys method for ruby 2.4
    
    * Disable Style/HashTransformKeys as ruby 2.4 has no method 'transform_keys'
    
    * Disable cop Metrics/ParameterLists for ProcessingJobUrlBuilder
    
    * Replace :slice method with :select for ruby 2.4
    
    * Fix specs and rubocop offenses
    
    * Replace non-existing methods for ruby 2.4
    
    * Add spec to check if the video original_source includes all the params
    
    * Add request for video conversion status with specs
    
    * Remove param validators
    
    * Remove validators from VideoConversionClient
    
    * Allow hash param for single video processing
    
    * Fix conflicts and specs
    
    * Update video conversion status specs
    
    * Add errors raising in case of problems/error is present in successfull server response

--- lib/uploadcare/client/conversion/video_conversion_client.rb
+++ lib/uploadcare/client/conversion/video_conversion_client.rb
@@ -2,6 +2,7 @@
 
 require_relative '../rest_client'
 require 'param/conversion/video/processing_job_url_builder'
+require 'exception/conversion_error'
 
 module Uploadcare
   module Client
@@ -15,6 +16,10 @@ module Uploadcare
           post(uri: '/convert/video/', content: body)
         end
 
+        def get_conversion_status(token)
+          get(uri: "/convert/video/status/#{token}/")
+        end
+
         def headers
           {
             'Content-type': 'application/json',
@@ -24,6 +29,23 @@ module Uploadcare
         end
 
         private
+
+        def success(response)
+          body = response.body.to_s
+          result = extract_result(body)
+
+          Dry::Monads::Success(result)
+        end
+
+        def extract_result(response_body)
+          return nil if response_body.nil? || response_body.empty?
+
+          parsed_body = JSON.parse(response_body, symbolize_names: true)
+          errors = parsed_body[:error] || parsed_body[:problems]
+          raise ConversionError, errors unless errors.nil? || errors.empty?
+
+          parsed_body
+        end
 
         # Prepares body for convert_many method
         def build_body_for_many(arr, options)
