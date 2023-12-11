eb0f5206afd442f8fc5a17b86a8f4d407f08fb92
Josh Schwartzman <josh@dreamseedo.org>
2023-12-10 23:22:44 -0700

    When polling, raise if an error is returned (#142)
    Previously, if an error from the API was returned while polling for updates on a file, it would continue polling. I updated it so that it raises a RequestError if the API returns an error status. If not, it raises a RetryError, which is swallowed by the retry gem's `with_retries` block.
    
    Example of an error that led to continued polling:
    
    ```
    {"status":"error","error":"File validation error: Uploading of these file types is not allowed.","error_code":"DownloadFileValidationFailedError"}
    ```

--- lib/uploadcare/client/uploader_client.rb
+++ lib/uploadcare/client/uploader_client.rb
@@ -75,12 +75,22 @@ module Uploadcare
       def poll_upload_response(token)
         with_retries(max_tries: Uploadcare.config.max_request_tries,
                      base_sleep_seconds: Uploadcare.config.base_request_sleep,
-                     max_sleep_seconds: Uploadcare.config.max_request_sleep) do
+                     max_sleep_seconds: Uploadcare.config.max_request_sleep,
+                     rescue: RetryError) do
           response = get_upload_from_url_status(token)
-          raise RequestError if %w[progress waiting unknown].include?(response.success[:status])
-
-          response
+          handle_polling_response(response)
         end
+      end
+
+      def handle_polling_response(response)
+        case response.success[:status]
+        when 'error'
+          raise RequestError, response.success[:error]
+        when 'progress', 'waiting', 'unknown'
+          raise RetryError, response.success[:error]
+        end
+
+        response
       end
 
       # Prepares body for upload_many method
