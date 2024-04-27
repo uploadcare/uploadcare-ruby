e309a89faad74d0d9faf115d3c8e3e530151b357
Vipul A M <vipulnsward@gmail.com>
2024-04-27 20:23:58 +0530

    [gh-134] Fix hmacs in generated spec urls

--- spec/uploadcare/signed_url_generators/akamai_generator_spec.rb
+++ spec/uploadcare/signed_url_generators/akamai_generator_spec.rb
@@ -20,7 +20,7 @@ module Uploadcare
 
       context 'when acl not present' do
         it 'returns correct url' do
-          expected_url = 'https://example.com/a7d5645e-5cd7-4046-819f-a6a2933bafe3/?token=exp=1649343900~acl=/a7d5645e-5cd7-4046-819f-a6a2933bafe3/~hmac=a82d0068adeb2fc5ecf87e7210fe537d234940807725f982dd6c776cbd24df3a'
+          expected_url = 'https://example.com/a7d5645e-5cd7-4046-819f-a6a2933bafe3/?token=exp=1649343900~acl=/a7d5645e-5cd7-4046-819f-a6a2933bafe3/~hmac=d8b4919d595805fd8923258bb647065b7d7201dad8f475d6f5c430e3bffa8122'
           expect(subject.generate_url(uuid)).to eq expected_url
         end
       end
@@ -29,7 +29,7 @@ module Uploadcare
         let(:uuid) { "#{super()}/-/resize/640x/other/transformations/" }
 
         it 'returns correct url' do
-          expected_url = 'https://example.com/a7d5645e-5cd7-4046-819f-a6a2933bafe3/-/resize/640x/other/transformations/?token=exp=1649343900~acl=/a7d5645e-5cd7-4046-819f-a6a2933bafe3/-/resize/640x/other/transformations/~hmac=7517f479d4413225b48b91f51b83c13f26c1e690adc72dff4dcf627e23d7f676'
+          expected_url = 'https://example.com/a7d5645e-5cd7-4046-819f-a6a2933bafe3/-/resize/640x/other/transformations/?token=exp=1649343900~acl=/a7d5645e-5cd7-4046-819f-a6a2933bafe3/-/resize/640x/other/transformations/~hmac=64dd1754c71bf194fcc81d49c413afeb3bbe0e6d703ed4c9b30a8a48c1782f53'
           expect(subject.generate_url(uuid)).to eq expected_url
         end
       end
@@ -37,7 +37,7 @@ module Uploadcare
       context 'when acl present' do
         it 'returns correct url' do
           acl = '/*/'
-          expected_url = 'https://example.com/a7d5645e-5cd7-4046-819f-a6a2933bafe3/?token=exp=1649343900~acl=/*/~hmac=06ab92c7ae863f7d6375d9fd28aa02bd7ca1cab9a10a14653b6cf6ea4f5170b2'
+          expected_url = 'https://example.com/a7d5645e-5cd7-4046-819f-a6a2933bafe3/?token=exp=1649343900~acl=/*/~hmac=984914950bccbfe22f542aa1891300fb2624def1208452335fc72520c934c4c3'
           expect(subject.generate_url(uuid, acl)).to eq expected_url
         end
       end
@@ -50,7 +50,7 @@ module Uploadcare
 
       context 'when wildcard is true' do
         it 'returns correct url' do
-          expected_url = 'https://example.com/a7d5645e-5cd7-4046-819f-a6a2933bafe3/?token=exp=1649343900~acl=/a7d5645e-5cd7-4046-819f-a6a2933bafe3/*~hmac=f0ae3655fb66376a172c29978c0e3db23c1359aec636c7f3395a165520d84d54'
+          expected_url = 'https://example.com/a7d5645e-5cd7-4046-819f-a6a2933bafe3/?token=exp=1649343900~acl=/a7d5645e-5cd7-4046-819f-a6a2933bafe3/*~hmac=6f032220422cdaea5fe0b58f9dcf681269591bb5d1231aa1c4a38741d7cc2fe5'
           expect(subject.generate_url(uuid, nil, wildcard: true)).to eq expected_url
         end
       end
