c39b82cd431f88b3f4673f54fa66772a13402b1d
Alex Gusev <alex.gusev@uploadcare.com>
2026-05-01 13:21:29 +0600

    Fix file handling style

--- api_examples/upload_api/post_multipart_complete.rb
+++ api_examples/upload_api/post_multipart_complete.rb
@@ -4,5 +4,6 @@ require 'uploadcare'
 Uploadcare.config.public_key = 'YOUR_PUBLIC_KEY'
 Uploadcare.config.secret_key = 'YOUR_SECRET_KEY'
 
-source_file = File.open('image.png')
-Uploadcare::Uploader.upload(source_file, store: 'auto')
+File.open('image.png') do |source_file|
+  Uploadcare::Uploader.upload(source_file, store: 'auto')
+end
