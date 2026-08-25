c9a516b4ae0f676b4348c4604fe04c61bc4ebfaf
Vipul A M <vipulnsward@gmail.com>
2026-08-25 18:16:03 +0530

    Simplify file search operation

--- lib/uploadcare/operations/file_search.rb
+++ lib/uploadcare/operations/file_search.rb
@@ -4,41 +4,32 @@
 class Uploadcare::Operations::FileSearch
   QUERY_OPTIONS = %i[limit offset include].freeze
 
-  def self.call(options:, client:, resource_class:, request_options: {})
-    new(options: options, client: client, resource_class: resource_class, request_options: request_options).call
-  end
-
-  def initialize(options:, client:, resource_class:, request_options: {})
-    @options = options
-    @client = client
-    @resource_class = resource_class
-    @request_options = request_options
-  end
-
-  def call
-    response = Uploadcare::Result.unwrap(
-      client.api.rest.files.search(
-        params: search_params, query: query_params, request_options: request_options
+  class << self
+    def call(options:, client:, resource_class:, request_options: {})
+      search_params, query_params = split_options(options)
+      response = Uploadcare::Result.unwrap(
+        client.api.rest.files.search(
+          params: search_params, query: query_params, request_options: request_options
+        )
       )
-    )
 
-    build_result(response)
-  end
+      Uploadcare::Collections::FileSearchResult.new(
+        resources: response.fetch('results', []).map { |data| resource_class.new(data, client) },
+        next_page: response['next'],
+        previous_page: response['previous'],
+        per_page: response['per_page'],
+        total: response['total'],
+        api_client: client.api.rest.files,
+        resource_class: resource_class,
+        client: client,
+        request_options: request_options,
+        search_params: search_params
+      )
+    end
 
-  private
+    private
 
-  attr_reader :client, :options, :request_options, :resource_class
-
-  def search_params
-    @search_params ||= split_options.first
-  end
-
-  def query_params
-    @query_params ||= split_options.last
-  end
-
-  def split_options
-    @split_options ||= begin
+    def split_options(options)
       body = options.dup
       query = QUERY_OPTIONS.to_h do |key|
         value = body.key?(key) ? body.delete(key) : body[key.to_s]
@@ -47,20 +38,5 @@ class Uploadcare::Operations::FileSearch
       end
       [body, query.compact]
     end
-  end
-
-  def build_result(response)
-    Uploadcare::Collections::FileSearchResult.new(
-      resources: response.fetch('results', []).map { |data| resource_class.new(data, client) },
-      next_page: response['next'],
-      previous_page: response['previous'],
-      per_page: response['per_page'],
-      total: response['total'],
-      api_client: client.api.rest.files,
-      resource_class: resource_class,
-      client: client,
-      request_options: request_options,
-      search_params: search_params
-    )
   end
 end
